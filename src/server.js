0838b467e6da0446d0bfb91d4f8800a364527fff
satyamKumar <satyamkumar2302@gmail.com>
2022-04-08 14:29:58 +0530

    create server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,35 @@
-const express = require("express");
+import express from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+import { v4 as uuid } from "uuid";
+
 const app = express();
+const server = new http.Server(app);
+const socket = new SocketServer(server);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res)=> {
-  res.send("hello");
+  const redirectURL = uuid();
+  res.redirect(`/${redirectURL}`);
+})
+
+app.get("/:room", (req, res)=> {
+  const roomID = req.params.room;
+  res.render("room", {
+    roomID,
+  });
+})
+
+socket.on("connection", connection => {
+  connection.on("join-room", (roomId, userId) => {
+    console.log(roomId);
+    console.log(userId);
+  })
 })
 
 app.listen(5000, () => {
   console.log("listening on 500");
 })
+
